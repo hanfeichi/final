3db0762e3d17a58a3ab9b04f85b674673465f6ba
rirone <https://github.com/rirone/final.git>
2020-01-07 00:03:57 -0800

    1

---
+++ ex3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("The program needs at least one integer parameter to run!\n");
+        return -1;
+    }
+    if (!isdigit(argv[1]))
+    {
+        printf("The parameter has to be an integer format!\n");
+        return -1;
+    }
+    int min;
+    min = atof(argv[1]);
+    for (int i = 1; i < argc; i++)
+    {
+        if (!isdigit(argv[i]))
+        {
+            printf("The parameter has to be an integer format!\n");
+            return -1;
+        }
+        if (atof(argv[i]) < min)
+            min = atof(argv[i]);
+    }
+    printf("Min parameter is %d\n", min);
+}
